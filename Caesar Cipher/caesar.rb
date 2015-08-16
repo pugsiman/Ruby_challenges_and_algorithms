b68675c5979d1f08a725380eceef5c38c07bfa1c
Aviv Levinski <pugsiman@gmail.com>
2015-08-16 14:55:23 +0300

    adding Caesar cipher

---
+++ Caesar Cipher/caesar.rb
@@ -0,0 +1,21 @@
+def caesar_cipher(text, shift)
+	alphabet = ("a".."z").to_a
+	
+	result_text = ""
+	
+	text.split("").each do |char|
+		if "!?., ".include?(char)
+			result_text << char
+		else
+			ciphered_char = alphabet[(alphabet.index(char.downcase) + shift)]
+			if char == char.upcase
+				result_text << ciphered_char.upcase
+			else
+				result_text << ciphered_char
+			end
+		end
+	end
+	return result_text.strip
+end
+
+print caesar_cipher("What a string!", 5)
