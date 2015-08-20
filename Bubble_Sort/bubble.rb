942002b318a17e1139cbc0b1b544f4f6bd9a869f
Aviv Levinski <pugsiman@gmail.com>
2015-08-20 18:45:07 +0300

    bubble_sort

---
+++ Bubble_Sort/bubble.rb
@@ -0,0 +1,32 @@
+def bubble_sort(array)
+	loop do
+		bubble_sorted = true
+		# using for-loop to avoid iterate over non-existent value, causing error.
+		for a in (0...(array.length-1))
+			if array[a] > array[a+1]
+				bubble_sorted = false
+				array[a], array[a+1] = array[a+1], array[a]
+			end
+		end
+	break if bubble_sorted
+	end
+	array
+end
+
+print bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(array)
+	loop do
+		bubble_sorted=true
+		for a in (0..(array.length-2) )
+			if (yield(array[a],array[a+1])) && (yield(array[a],array[a+1])) > 0
+				sorted = false
+				array[a], array[a+1] = array[a+1], array[a]
+			end
+		end
+		break if bubble_sorted
+	end
+	return array
+end
+
+print bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length}
