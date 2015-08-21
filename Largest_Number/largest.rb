618534a6b24f263ce2300ef1c9ee4eec3c62b84a
Aviv Levinski <pugsiman@gmail.com>
2015-08-21 10:57:44 +0300

    Largest_number

---
+++ Largest_Number/largest.rb
@@ -0,0 +1,27 @@
+#with "magic" (quicksort)
+
+def largest_num num
+	splited = num.to_s.split("")
+	splited.sort {|a,b| b <=> a}.join.to_i
+end
+
+p largest_num 257
+
+#without magic
+
+def largest_without_magic num
+	splited = num.to_s.split("")
+	loop do
+		sorted = true
+		for a in (0...(splited.length-1))
+			if splited[a] < splited[a+1]
+				sorted = false
+				splited[a], splited[a+1] = splited[a+1], splited[a]
+			end
+		end
+	break if sorted
+	end
+	splited.join.to_i
+end
+
+p largest_without_magic 20574
