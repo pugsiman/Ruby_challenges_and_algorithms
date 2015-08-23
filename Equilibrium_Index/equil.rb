ef5a74868dd026983861df0c89a8b50ea5c1a475
Aviv Levinski <pugsiman@gmail.com>
2015-08-23 13:11:42 +0300

    added equilibrium index

--- Equilibrium_Index/equil.rb
+++ Equilibrium_Index/equil.rb
@@ -1,12 +1,7 @@
 def equil(array)
-	indices = []
-	for a in (0..(array.length-1))
-		if array[0..a] = array[a..(array.length-1)]
-			indices << a
-			p a
-		end
+	array.each_index.select do |i|
+		array[0...i].inject(:+) == array[i+1..-1].inject(:+)
 	end
-	indices
 end
 
-equil [-7, 1, 5, 2, -4, 3, 0]
+p equil [-7, 1, 5, 2, -4, 3, 0]
