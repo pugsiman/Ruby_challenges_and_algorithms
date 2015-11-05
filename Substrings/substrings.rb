35aaadebf5e950d333d2860ed8ac9420fda07985
Aviv Levinsky <pugsiman@gmail.com>
2015-11-05 17:05:33 +0200

    fix indents

--- Substrings/substrings.rb
+++ Substrings/substrings.rb
@@ -1,14 +1,11 @@
 def substrings(counted_word, words_array)
   words_listing = Hash.new(0)
-
   words_array.each do |word|
     counted_word.downcase.scan(word.downcase) { words_listing[word] += 1 }
   end
-
   words_listing
 end
 
 dictionary = %w(below down go going horn how howdy it i low own part partner sit)
-
 print substrings('below', dictionary)
 print substrings("Howdy partner, sit down! How's it going?", dictionary)
